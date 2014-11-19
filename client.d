869c65aac1e8a5f4669e4cb2337cb92908301c83
Orlando Borges Botelho <orlando2bjr@outlook.com>
2014-11-19 07:20:53 -0200

    Cliente e servidor integrados na classe

--- client.d
+++ client.d
@@ -1,4 +1,4 @@
-import std.stdio, std.socket, oxcli, std.conv;
+import std.stdio, std.socket, oxcli, std.conv, std.string;
 
 version(Windows)
 {
@@ -14,15 +14,85 @@ int main(string[] argv)
 		//if(SetConsoleOutputCP(65001) == 0)
 		//    throw new Exception("falha");
 	}
-	auto socket = new Socket(AddressFamily.INET, SocketType.STREAM);
+	auto game = new OX();
+
+	// tentar conexão
+	Socket socket;
 	char[1024] buffer;
-	socket.connect(new InternetAddress("localhost", 2525));
-	// ox test
+	auto isNodeTypeUndefined = true;
+	auto serverAddress = new InternetAddress(2525);
+	// definir modo de comunicação
+	while(isNodeTypeUndefined)
+	{
+		try
+		{
+			socket = new Socket(AddressFamily.INET, SocketType.STREAM);
+
+			socket.connect(serverAddress);
+			isNodeTypeUndefined = false;
+		}
+		catch (Exception e)
+		{
+			writeln("Erro: %s", e.msg);
+			writeln("Não consegui conectar em ", serverAddress.toAddrString(), ":", serverAddress.toPortString(), ", o que deseja fazer?");
+			writeln("1) Iniciar como servidor na máquina.");
+			writeln("2) Iniciar como servidor na rede local.");
+			writeln("3) Tentar novamente em ", serverAddress.toAddrString(), ".");
+			writeln("4) Alterar endereço do servidor.");
+			int chosenOption;
+			readf(" %d\n", &chosenOption);
+			switch(chosenOption)
+			{
+				default:
+				case 1:
+					socket = new TcpSocket;
+					assert(socket.isAlive);
+					serverAddress = new InternetAddress("localhost", 2525);
+					socket.bind(serverAddress);
+					socket.listen(1);
+					writeln("Servidor ouvindo em ", serverAddress.toAddrString(), ":",  serverAddress.toPortString());
+					break;
+				case 2:
+					socket = new TcpSocket;
+					assert(socket.isAlive);
+					socket.bind(new InternetAddress(2525));
+					socket.listen(1);
+					writeln("Servidor ouvindo em ", serverAddress.toAddrString(), ":",  serverAddress.toPortString());
+					break;
+				case 3:
+					break;
+				case 4:
+					auto isIPinvalid = true;
+					string newAddress;
+					while (isIPinvalid)
+					{
+						write("Digite outro endereço IP do servidor (e.g.: 123.123.222.111 ou localhost):\n>");
+						newAddress = readln().chomp();
+						if(newAddress == "localhost")
+							isIPinvalid = false;
+						else
+						{
+							try
+							{
+								Address address = parseAddress(newAddress);
+								isIPinvalid = false;
+							}
+							catch (SocketException e)
+							{
+								writefln("  %s não é um endereço IP válido: %s",
+										 newAddress, e.msg);
+							}
+						}
+					}
+					serverAddress = new InternetAddress(newAddress, 2525);
+					break;
+			}
+		}
+	}
 
 	auto received = socket.receive(buffer);
 	// aguarde ACK
 	//writeln("O servidor disse: ", buffer[0 .. received]);
-	auto game = new OX(to!int(buffer[0 .. received]));
 	auto isRunning = true;
 	socket.blocking = false;
 	while(isRunning)
--- oxcli.d
+++ oxcli.d
@@ -1,7 +1,4 @@
-import std.stdio;
-import std.string;
-import std.random;
-import std.conv;
+import std.stdio, std.string, std.random, std.conv, std.socket;
 // http://ddili.org/ders/d.en/logical_expressions.html
 version(Windows)
 {
@@ -55,6 +52,25 @@ version(Windows)
 //    return 0;
 //}
 
+class OX
+{
+	// jeitinho para o console do Windows funcionar com acentos
+	version(Windows)
+	{
+		int consoleCP;
+		void consoleFixBegin() // para consertar acentos do código fonte
+		{
+			// definir saí­da como código de página UTF8
+			if(SetConsoleOutputCP(65001) == 0)
+				throw new Exception("falha na definição do código de página do console como UTF8");
+		}
+		void consoleFixEnd() // para aceitar acentos da entrada do usuário
+		{
+			// definir saí­da como código de página padrão
+			if(SetConsoleOutputCP(consoleCP) == 0)
+				throw new Exception("falha na definição do código de página do console como padrão inicial");
+		}
+	}
 	enum Occupant: string
 	{
 		None = " ",
@@ -66,44 +82,162 @@ version(Windows)
 		O = Occupant.O,
 		X = Occupant.X
 	}
-class OX
-{
+	enum NodeType
+	{
+		Server,
+		Client
+	}
 	Occupant[9] board;
 	string[9] available;
 	size_t positionIndex;
 	Player[2] players = [Player.O, Player.X];
 	int turnPlayer;
-
-	// jeitinho para o console do Windows funcionar com acentos
-	version(Windows)
+	Socket server;
+	Socket client;
+	char[1024] buffer;
+	auto isNodeTypeUndefined = true;
+	NodeType nodeType = void;
+	auto address = "localhost";
+	InternetAddress serverAddress;
+	this()
 	{
-		void setConsoleCPUTF8()
-		{
-			// definir saí­da como código de página UTF8
-			if(SetConsoleOutputCP(65001) == 0)
-				throw new Exception("falha na definição do código de página do console como UTF8");
-		}
-		void resetConsoleCP()
-		{
-			// definir saí­da como código de página padrão
-			if(SetConsoleOutputCP(consoleCP) == 0)
-				throw new Exception("falha na definição do código de página do console como padrão inicial");
-		}
-	}
-
-	void init()
-	{
+		serverAddress = new InternetAddress("localhost", 2525);
 		// guardar valor inicial do código de página do console
 		version(Windows)
 		{
-			int consoleCP = GetConsoleOutputCP();
+			consoleCP = GetConsoleOutputCP();
 		}
-
+		// marcar número das posições no tabuleiro
 		foreach(position; available)
 		{
 			position = new string(positionIndex);
 			positionIndex++;
 		}
+		// definir modo de conexão
+		while(isNodeTypeUndefined)
+		{
+			// tentar conexão local
+			try
+			{
+				server = new Socket(AddressFamily.INET, SocketType.STREAM);
+				consoleFixBegin();
+					writeln("Tentando conectar em: ", serverAddress.toAddrString(), ".");
+				consoleFixEnd();
+				server.connect(serverAddress);
+				nodeType = NodeType.Client;
+				consoleFixBegin();
+					writeln("Conectado em: ", serverAddress.toAddrString(), ".");
+				consoleFixEnd();
+				isNodeTypeUndefined = false;
+			}
+			catch (Exception e)
+			{
+				consoleFixBegin();
+					writeln("Erro: %s", e.msg);
+					writeln("Não consegui conectar em ", serverAddress.toAddrString(), ":", serverAddress.toPortString(), ", o que deseja fazer?");
+					writeln("1) Iniciar como servidor na máquina.");
+					writeln("2) Iniciar como servidor na rede local.");
+					writeln("3) Tentar novamente em ", serverAddress.toAddrString(), ".");
+					writeln("4) Alterar endereço do servidor.");
+				consoleFixEnd();
+				int chosenOption;
+				readf(" %d\n", &chosenOption);
+				switch(chosenOption)
+				{
+					default: break;
+					case 1:
+						startServer(new InternetAddress("localhost", 2525));
+						isNodeTypeUndefined = false;
+						//nodeType = NodeType.Server; // servidor, não tentar mais conectar
+						//client = new TcpSocket;
+						//assert(client.isAlive);
+						//serverAddress = new InternetAddress("localhost", 2525);
+						//socket.bind(serverAddress);
+						//socket.listen(1);
+						//consoleFixBegin();
+						//    writeln("Servidor ouvindo em ", serverAddress.toAddrString(), ":",  serverAddress.toPortString());
+						//consoleFixEnd();
+						break;
+					case 2:
+						startServer(new InternetAddress(2525));
+						isNodeTypeUndefined = false;
+						//nodeType = NodeType.Server; // servidor, não tentar mais conectar
+						//socket = new TcpSocket;
+						//assert(socket.isAlive);
+						//socket.bind(new InternetAddress(2525));
+						//socket.listen(1);
+						//consoleFixBegin();
+						//    writeln("Servidor ouvindo em ", serverAddress.toAddrString(), ":",  serverAddress.toPortString());
+						//consoleFixEnd();
+						break;
+					case 3:
+						nodeType = NodeType.Client;
+						break;
+					case 4:
+						nodeType = NodeType.Client;
+						auto isIPinvalid = true;
+						string newAddress;
+						while (isIPinvalid)
+						{
+							consoleFixBegin();
+								write("Digite outro endereço IP do servidor (e.g.: 123.123.222.111 ou localhost):\n>");
+							consoleFixEnd();
+							newAddress = readln().chomp();
+							if(newAddress == "localhost")
+								isIPinvalid = false;
+							else
+							{
+								try
+								{
+									Address address = parseAddress(newAddress);
+									isIPinvalid = false;
+								}
+								catch (SocketException e)
+								{
+									consoleFixBegin();
+										writefln("  %s não é um endereço IP válido: %s",
+											 newAddress, e.msg);
+									consoleFixEnd();
+								}
+							}
+						}
+						serverAddress = new InternetAddress(newAddress, 2525);
+						break;
+				}
+			}
+		}
+
+		switch(nodeType)
+		{
+			case NodeType.Server:
+				break;
+			case NodeType.Client:
+				break;
+			default: break;
+		}
+
+		auto rng = Random(unpredictableSeed);
+		turnPlayer = uniform(0, 2, rng);
+		printCurrentPlayer();
+	}
+	void startServer(InternetAddress serverAddress)
+	{
+		nodeType = NodeType.Server; // servidor, não tentar mais conectar
+		Socket listener = new TcpSocket;
+		assert(listener.isAlive);
+		serverAddress = new InternetAddress("localhost", 2525);
+		listener.bind(serverAddress);
+		listener.listen(1);
+		consoleFixBegin();
+			writeln("Servidor ouvindo em ", serverAddress.toAddrString(), ":",  serverAddress.toPortString());
+		consoleFixEnd();
+		client = listener.accept();
+		consoleFixBegin();
+			writeln("Aceitei conexão de ", client.remoteAddress().toAddrString(), ":",  serverAddress.toPortString());
+		consoleFixEnd();
+		listener.close();
+
+
 	}
 	void printCurrentPlayer()
 	{
@@ -112,19 +246,12 @@ class OX
 		else
 			writeln("Machados");
 	}
-	this()
-	{
-		init();
-		auto rng = Random(unpredictableSeed);
-		turnPlayer = uniform(0, 2, rng);
-		printCurrentPlayer();
-	}
-	this(int firstPlayer)
-	{
-		init();
-		turnPlayer = firstPlayer;
-		printCurrentPlayer();
-	}
+	//this(int firstPlayer)
+	//{
+	//    init();
+	//    turnPlayer = firstPlayer;
+	//    printCurrentPlayer();
+	//}
 	void writePosition(Occupant quadrant, size_t position)
 	
 	{
